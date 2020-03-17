40ddde19f6879fdaf54f67f858c9b6e0a99f0036
davidnardya@gmail.com <`1q`1q~!Q>
2020-03-17 14:47:59 +0200

    Tasks 6 & 7: Added adapter and layout manager

---
+++ app/src/main/java/com/hackeru/todolist/AddTaskAdapter.java
@@ -0,0 +1,60 @@
+package com.hackeru.todolist;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hackeru.todolist.pojo.Task;
+
+import java.util.ArrayList;
+
+public class AddTaskAdapter extends RecyclerView.Adapter<AddTaskAdapter.TasksViewHolder> {
+
+    ArrayList<Task> taskArrayist;
+
+    public AddTaskAdapter(ArrayList<Task> taskArrayist) {
+        this.taskArrayist = taskArrayist;
+    }
+
+    @NonNull
+    @Override
+    public TasksViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_task_layout, parent, false);
+
+        return new TasksViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TasksViewHolder holder, int position) {
+        Task item = taskArrayist.get(position);
+        holder.taskNameTv.setText(item.getTaskName());
+        holder.taskDescriptionTv.setText(item.getTaskDescription());
+        DataManager.addNewTask(taskArrayist.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskArrayist.size();
+    }
+
+    public class TasksViewHolder extends RecyclerView.ViewHolder {
+
+        RecyclerView recyclerView;
+        TextView taskNameTv;
+        TextView taskDescriptionTv;
+
+
+        public TasksViewHolder(@NonNull View itemView) {
+            super(itemView);
+            recyclerView = itemView.findViewById(R.id.main_container);
+            taskNameTv = itemView.findViewById(R.id.task_name_tv);
+            taskDescriptionTv = itemView.findViewById(R.id.task_description_tv);
+
+        }
+    }
+}
